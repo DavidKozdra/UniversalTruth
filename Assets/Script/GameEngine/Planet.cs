1b172e1e269c0fcd16eed34e8606dc06c77ab384
DavidKozdra <45106564+DavidKozdra@users.noreply.github.com>
2019-11-26 08:29:58 -0500

    Scaleing

--- Assets/Script/GameEngine/Planet.cs
+++ Assets/Script/GameEngine/Planet.cs
@@ -18,13 +18,15 @@ public class Event
 public class Planet : MonoBehaviour
 {
 
-    public GameObject Medal, Highlight,ButtonTemplate;
+    public GameObject Medal, Highlight,BuyDisplay,MineB,Enslave,Trade,Harvest, MainDisplay;
     public string Name;
-    public int Reasource;
-    [SerializeField] Event[] Events;
+    public int Reasource,Life;
+    [SerializeField] List<Event> Events = new List<Event>();
     public bool Owned;
+    public int Cost;
     public static bool Selected;
     public Player p => FindObjectOfType<Player>();
+    public Text Costtext;
 
     int rand(int Min, int Max)
     {
@@ -34,39 +36,64 @@ public class Planet : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+
         foreach (Event e in Events)
         {
-            GameObject button = Instantiate(ButtonTemplate) as GameObject;
-            button.SetActive(true);
-            button.GetComponent<ButtonType>().Display.text = e.Description;
-            button.GetComponent<ButtonType>().I = e.Icon;
-            button.transform.SetParent(ButtonTemplate.transform.parent, false);
+
         }
+
         //Display = GameObject.Find("PDis");
         //NameText = GameObject.Find("Name").GetComponent<Text>(); 
         //InfoText = GameObject.Find("Info").GetComponent<Text>(); 
-        if (Reasource == 0) {
+        if (Reasource == 0)
+        {
             Reasource = rand(1, 200);
+            Life = rand(0,rand(0,200));
         }
-        if (Owned) {
+        if (Owned)
+        {
             Buy();
         }
     }
 
     public void Buy()
     {
+        if (p.Currency>= Cost) {
+            p.Currency -= Cost;
+            p.AddText.gameObject.SetActive(true);
+            p.AddText.text = "-" + Cost;
+            Medal.SetActive(Owned);
+            p.OwnedPlanets.Add(gameObject);
+            Owned = true;
+        }
 
-        p.OwnedPlanets.Add(gameObject);
-        Owned = true;
+    }
+    public void Mine() {
+        if (Reasource>=50) {
+            MineB.SetActive(true);
+            Reasource -= 50;
+        }
 
     }
 
-
+    public void CalculateMass(){
+        int mass = Reasource - 90;
+        if (transform.localScale.x <= (mass)/2)
+        {
+            if (transform.localScale.x <= 11)
+            {
+                transform.localScale += new Vector3(.1f, .1f, 0);
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-
-
+        if (p.Timer<=0&&Life>=2) {
+            Life++;
+        }
+        CalculateMass();
+        MainDisplay.SetActive(!BuyDisplay.activeSelf);
 
         if (Reasource <= 0) {
             //Died
--- Assets/Script/GameEngine/SUN.cs
+++ Assets/Script/GameEngine/SUN.cs
@@ -6,11 +6,14 @@ public class SUN : Planet {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        if (Owned)
+        {
+            Buy();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        CalculateMass();
+    }
 }
